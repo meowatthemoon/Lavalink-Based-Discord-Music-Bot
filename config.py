77a7ba5919d4462706a43e3b99b4050ad37c295d
meowatthemoon <34717240+meowatthemoon@users.noreply.github.com>
2024-07-12 22:15:46 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = 'YOUR_BOT_TOKEN_HERE'
+
+LAVALINK_HOST = "127.0.0.1" # Lavalink server IP (must be the same as application.yml)
+LAVALINK_PORT = 2333 # Lavalink server port (must be the same as application.yml)
+LAVALINK_PASSWORD = 'youshallnotpass' # Lavalink server password (must be the same as application.yml)
+
+DB_PATH = './database.db' # Name of the Database file
+MAX_NUM_PLAYLISTS = 10 # Max number of playlists for each discord user
+
+SELECTION_WAIT_TIME = 5
